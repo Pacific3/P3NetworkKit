9a123d9b47be04e7439803a07d8214959f509872
Oscar Swanros <oscar@swanros.com>
2016-06-19 14:04:50 -0500

    Create HTTPMethod enum

---
+++ Sources/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  P3NetworkKit
+//
+//  Created by Oscar Swanros on 6/19/16.
+//  Copyright © 2016 Pacific3. All rights reserved.
+//
+
+public enum HTTPMethod: String {
+    case GET    = "GET"
+    case POST   = "POST"
+    case PATCH  = "PATCH"
+    case PUT    = "PUT"
+    case DELETE = "DELETE"
+}
