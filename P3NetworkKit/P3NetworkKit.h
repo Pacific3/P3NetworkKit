d1264814c2017784f428c899918ded9a49678c00
Oscar Swanros <oscar@swanros.com>
2016-06-19 12:20:33 -0500

    Initial commit

---
+++ P3NetworkKit/P3NetworkKit.h
@@ -0,0 +1,19 @@
+//
+//  P3NetworkKit.h
+//  P3NetworkKit
+//
+//  Created by Oscar Swanros on 6/19/16.
+//  Copyright © 2016 Pacific3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for P3NetworkKit.
+FOUNDATION_EXPORT double P3NetworkKitVersionNumber;
+
+//! Project version string for P3NetworkKit.
+FOUNDATION_EXPORT const unsigned char P3NetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <P3NetworkKit/PublicHeader.h>
+
+
