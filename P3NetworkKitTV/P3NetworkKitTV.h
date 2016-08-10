4397e5e23015e2e85241ccbd1160c9301a433fde
Oscar Swanros <oscar@swanros.com>
2016-08-10 12:06:59 -0500

    Create P3NetworkKit target

---
+++ P3NetworkKitTV/P3NetworkKitTV.h
@@ -0,0 +1,19 @@
+//
+//  P3NetworkKitTV.h
+//  P3NetworkKitTV
+//
+//  Created by Oscar Swanros on 8/10/16.
+//  Copyright © 2016 Pacific3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for P3NetworkKitTV.
+FOUNDATION_EXPORT double P3NetworkKitTVVersionNumber;
+
+//! Project version string for P3NetworkKitTV.
+FOUNDATION_EXPORT const unsigned char P3NetworkKitTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <P3NetworkKitTV/PublicHeader.h>
+
+
