8c44689589ae0809c7ab4733a653406ef8c44473
Oscar Swanros <oscar@swanros.com>
2017-05-06 17:42:08 -0500

    Refactor image caching, expose P3ImageCache API

--- Sources/Extensions/UIImageView.swift
+++ Sources/Extensions/UIImageView.swift
@@ -7,117 +7,66 @@
 //
 
 public let kP3NKDidFinishSettingImageFromURLToImageView = "kP3NKDidFinishSettingImageFromURLToImageView"
-
-private func imageCacheKey(request: NSURLRequest) -> String {
-    return request.url?.absoluteString ?? ""
-}
-
-public protocol ImageCaching {
-    func cachedImage(request: NSURLRequest) -> UIImage?
-    func cache(image: UIImage, for request: NSURLRequest)
-}
-
-private class ImageCache: NSCache<AnyObject, AnyObject>, ImageCaching {
-    fileprivate func cache(image: UIImage, for request: NSURLRequest) {
-        setObject(image, forKey: imageCacheKey(request: request) as AnyObject)
-    }
-    
-    fileprivate func cachedImage(request: NSURLRequest) -> UIImage? {
-        switch request.cachePolicy {
-        case .reloadIgnoringLocalCacheData, .reloadIgnoringLocalAndRemoteCacheData:
-            return nil
-            
-        default:
-            break
-        }
-        
-        return object(forKey: imageCacheKey(request: request) as AnyObject) as? UIImage
-    }
-}
-
-private var OperationAssociatedObjectKey: UInt8 = 0
-private let session = URLSession(configuration: URLSessionConfiguration.ephemeral)
-private let operationQueue = P3OperationQueue()
+private var CacheIdentifierObjectKey: UInt8 = 0
 
 public extension UIImageView {
-    private static let p3_sharedImageCache = ImageCache()
-    
-    private var imageRequestOperation: P3Operation? {
+    private var downloadingOperationIdentifier: ImageCacheIdentifier? {
         get {
-            return objc_getAssociatedObject(self, &OperationAssociatedObjectKey) as? P3Operation
+            return objc_getAssociatedObject(self, &CacheIdentifierObjectKey) as? ImageCacheIdentifier
         }
         
         set {
-            objc_setAssociatedObject(self, &OperationAssociatedObjectKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self, &CacheIdentifierObjectKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
     
-    private func p3_setImage(request: URLRequest, placeholder: UIImage?) {
-        if let cachedImage = UIImageView.p3_sharedImageCache.cachedImage(request: request as NSURLRequest) {
+    public func p3_cancelImageRequestOperation() {
+        guard let id = downloadingOperationIdentifier else { return }
+        
+        P3ImageCache.sharedImageCache.cancelDownloadWithIdentifier(id: id)
+        downloadingOperationIdentifier = nil
+    }
+    
+    public func p3_setImageWithURL(url: URL) {
+        p3_setImage(url: url, placeholder: nil)
+    }
+    
+    public func p3_setImage(url: URL, placeholder: UIImage?) {
+        if let cachedImage = P3ImageCache.sharedImageCache.cachedImage(url: url) {
             image = cachedImage
-            imageRequestOperation = nil
             postNotification()
         } else {
             if let placeholder = placeholder {
                 image = placeholder
             }
             
-            let task = session.dataTask(with: request) { [weak self] data, response, error in
+            downloadingOperationIdentifier = P3ImageCache.sharedImageCache.downloadImageWithURLToCache(url: url, cachedImage: { [weak self] image in
                 p3_executeOnMainThread {
+                    self?.downloadingOperationIdentifier = nil
+                    
                     guard
                         let strongSelf = self,
-                        let data = data,
-                        let serializedImage = UIImage(data: data)
-                        else {
-                            return
-                    }
+                        let image = image
+                        else { return }
                     
                     UIView.transition(
                         with: strongSelf,
                         duration: 0.3,
                         options: .transitionCrossDissolve,
                         animations: {
-                            strongSelf.image = serializedImage
-                        }, completion: { finished in
-                            if finished {
-                                strongSelf.postNotification()
-                            }
+                            strongSelf.image = image
+                    }, completion: { (finished) in
+                        if finished {
+                            strongSelf.postNotification()
                         }
-                    )
-                    
-                    UIImageView.p3_sharedImageCache.cache(image: serializedImage, for: request as NSURLRequest)
+                    })
                 }
-            }
-            
-            imageRequestOperation = P3URLSessionTaskOperation(task: task)
-            #if os(iOS)
-                imageRequestOperation?.addObserver(observer: P3NetworkActivityObserver())
-            #endif
-            operationQueue.addOperation(imageRequestOperation!)
+            })
         }
     }
     
-    public func p_cancelImageRequestOperation() {
-        guard let operation = imageRequestOperation else {
-            return
-        }
-        
-        operation.cancel()
-    }
     
-    public func p_setImageWithURL(url: URL) {
-        p3_setImage(url: url, placeholder: nil)
-    }
-    
-    public func p3_setImage(url: URL, placeholder: UIImage?) {
-        var request = URLRequest(url: url)
-        request.addValue("image/*", forHTTPHeaderField: "Accept")
-        request.cachePolicy = .returnCacheDataElseLoad
-        
-        
-        p3_setImage(request: request, placeholder: placeholder)
-    }
-    
+    // MARK: - Private methods
     private func postNotification() {
         p3_executeOnMainThread {
             NotificationCenter.default.post(
---
+++ Sources/P3ImageCache.swift
@@ -0,0 +1,119 @@
+//
+//  P3ImageCache.swift
+//  P3NetworkKit
+//
+//  Created by Oscar Swanros on 5/6/17.
+//  Copyright © 2017 Pacific3. All rights reserved.
+//
+
+public typealias ImageCacheIdentifier = String
+
+public protocol ImageCaching {
+    func cachedImage(request: URLRequest) -> UIImage?
+    func cache(image: UIImage, for request: URLRequest)
+}
+
+final public class P3ImageCache: NSCache<AnyObject, AnyObject>, ImageCaching {
+    private let session = URLSession(configuration: URLSessionConfiguration.ephemeral)
+    private let operationQueue = P3OperationQueue()
+    private var downloadingOperations: [String:P3Operation] = [:]
+    
+    public static let sharedImageCache = P3ImageCache()
+    
+    // MARK: - Keys
+    public func imageCacheKey(request: URLRequest) -> ImageCacheIdentifier {
+        guard let url = request.url else {
+            return ""
+        }
+        
+        return imageCacheKey(url: url)
+    }
+    
+    public func imageCacheKey(url: URL) -> ImageCacheIdentifier {
+        return url.absoluteString
+    }
+    
+    
+    // MARK: - Getting from cache
+    public func cachedImage(request: URLRequest) -> UIImage? {
+        switch request.cachePolicy {
+        case .reloadIgnoringLocalCacheData, .reloadIgnoringLocalAndRemoteCacheData:
+            return nil
+            
+        default:
+            break
+        }
+        
+        return cachedImage(id: imageCacheKey(request: request))
+    }
+    
+    public func cachedImage(url: URL) -> UIImage? {
+        return cachedImage(id: imageCacheKey(url: url))
+    }
+    
+    private func cachedImage(id: ImageCacheIdentifier) -> UIImage? {
+        return object(forKey: id as AnyObject) as? UIImage
+    }
+    
+    
+    // MARK: - Adding to cache
+    public func cache(image: UIImage, for request: URLRequest) {
+        setObject(image, forKey: imageCacheKey(request: request) as AnyObject)
+    }
+    
+    public func cache(image: UIImage, url: URL) {
+        setObject(image, forKey: imageCacheKey(url: url) as AnyObject)
+    }
+    
+    public func cache(image: UIImage, string: String) {
+        setObject(image, forKey: string as AnyObject)
+    }
+    
+    @discardableResult
+    public func downloadImageWithURLToCache(url: URL, cachedImage: ((UIImage?) -> Void)?) -> ImageCacheIdentifier {
+        let request = imageRequestForURL(url: url)
+        let id = imageCacheKey(request: request)
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            self.downloadingOperations.removeValue(forKey: id)
+            
+            guard
+                let data = data,
+                let serializedImage = UIImage(data: data)
+                else {
+                    cachedImage?(nil)
+                    return
+            }
+            
+            P3ImageCache.sharedImageCache.cache(image: serializedImage, for: request)
+            cachedImage?(serializedImage)
+        }
+        
+        let imageRequestOperation = P3URLSessionTaskOperation(task: task)
+        #if os(iOS)
+            imageRequestOperation.addObserver(observer: P3NetworkActivityObserver())
+        #endif
+        downloadingOperations[id] = imageRequestOperation
+        operationQueue.addOperation(imageRequestOperation)
+        return id
+    }
+    
+    
+    // MARK: - Misc
+    public func cancelDownloadWithIdentifier(id: ImageCacheIdentifier) {
+        if let op = downloadingOperations[id] {
+            op.cancel()
+        }
+        
+        downloadingOperations[id] = nil
+    }
+    
+    
+    // MARK: - Private methods
+    private func imageRequestForURL(url: URL) -> URLRequest {
+        var request = URLRequest(url: url)
+        request.addValue("image/*", forHTTPHeaderField: "Accept")
+        request.cachePolicy = .returnCacheDataElseLoad
+        return request
+    }
+}
